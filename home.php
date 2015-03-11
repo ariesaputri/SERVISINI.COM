a7b34838e76c8f99a3e2210d5adf5cd270bd7941
ariesaputri <ariesaputri87@gmail.com>
2015-03-11 11:56:33 +0700

    Create home.php
    front end

---
+++ home.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!--
+Template Name: PlusBusiness
+Author: <a href="http://www.os-templates.com/">OS Templates</a>
+Author URI: http://www.os-templates.com/
+Licence: Free to use under our free template licence terms
+Licence URI: http://www.os-templates.com/template-terms
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>SERVISINI.COM</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link rel="stylesheet" href="layout/styles/layout.css" type="text/css" />
+<script type="text/javascript" src="layout/scripts/jquery.min.js"></script>
+<script type="text/javascript" src="layout/scripts/jquery.jcarousel.pack.js"></script>
+<script type="text/javascript" src="layout/scripts/jquery.jcarousel.setup.js"></script>
+</head>
+<body id="top">
+<!-- ####################################################################################################### -->
+<div class="wrapper col1">
+  <div id="header">
+    <div id="logo">
+      <h1><a href="index.html">servisini.com</a></h1>
+      <p>cari tempat servis motor sekarang lebih mudah dan cepat!</p>
+    </div>
+    <div id="topnav">
+      <ul>
+        <li class="active"><a href="index.html">Beranda</a></li>
+        <li><a href="pages/style-demo.html">Daftar Bengkel</a></li>
+        <li><a href="pages/full-width.html">Rekomendasi Editor</a></li>
+        <li><a href="#">Cari Berdasarkan</a>
+          <ul>
+            <li><a href="#">Lokasi</a></li>
+            	<ul>
+           		<li><a href="#">Lokasi</a></li>
+            <li><a href="#">Jenis Tempat Servis </a></li>
+          </ul>
+        <li><a href="pages/full-width.html">Hubungi Kami</a></li>
+          </ul>
+        </li>
+        <li class="last"><a href="login.php">Login</a></li>
+      </ul>
+    </div>
+    <br class="clear" />
+  </div>
+  <p>&nbsp;</p>
+</div>
+<!-- ####################################################################################################### -->
+<div class="wrapper col2">
+  <div id="featured_slide">
+    <div id="featured_content">
+      <ul>
+        <li><img src="images/bengkel/images (2).jpg" alt="" width="275" height="183" />
+          <div class="floater">
+            <h2>AHASS Kharisma Menur</h2>
+            <p>Servis motor lebih aman dan terpecaya, cuma di AHASS Kharisma Menur tempatnya! </p>
+            <p><em>&quot;Tempat servis luas, teknisi banyak. Anda tidak perlu menunggu lama.&quot;</em></p>
+            <p class="readmore"><a href="#">Selengkapnya &raquo;</a></p>
+          </div>
+        </li>
+        <li><img src="images/bengkel/YAMAHA/DSC00109.JPG" alt="" />
+          <div class="floater">
+            <h2>YAMAHA Aman Jaya Manyar</h2>
+           <p>Ingin tempat servis motor yang nyaman dan profesional? Disini tempatnya</p>
+            <p><em>&quot;Tempatnya nyaman dan didukung dengan teknisi yang cakap dan tanggap. Hasil servis memuaskan!&quot;</em></p>
+             <p class="readmore"><a href="#">Selengkapnya &raquo;</a></p>
+          </div>
+        </li>
+        <li><img src="images/bengkel/AHASS/arantan-4178-2.jpg" alt="" />
+          <div class="floater">
+            <h2>AHASS Putra Merdeka Keputih</h2>
+            <p>Buat kalian para mahasiswa ITS pengguna sepeda motor Honda, segera servis dan rawat sepeda motormu disini!</p>
+            <p><em>&quot;Tempat servis motor yang berlokasi di jalan Raya Menur ini  nyaman dan terpecaya.&quot;</em></p>
+            <p class="readmore"><a href="#">Selengkapnya &raquo;</a></p>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <a href="javascript:void(0);" id="featured-item-prev"><img src="layout/images/prev.png" alt="" /></a> <a href="javascript:void(0);" id="featured-item-next"><img src="layout/images/next.png" alt="" /></a> </div>
+</div>
+<!-- ####################################################################################################### -->
+<div class="wrapper col3">
+  <div id="container">
+    <div class="homepage">
+      <ul>
+        <li>
+          <h2><img src="images/demo/60x60.gif" alt="" />Nullamlacus dui ipsum conseque loborttis</h2>
+          <p>Nullamlacus dui ipsum conseque loborttis non euisque morbi penas dapibulum orna. Urnaultrices quis curabitur phasellentesque congue magnis vestibulum quismodo nulla et feugiat. Adipisciniapellentum leo ut consequam ris felit elit id nibh sociis malesuada.</p>
+          <p class="readmore"><a href="#">Selengkapnya &raquo;</a></p>
+        </li>
+        <li>
+          <h2><img src="images/demo/60x60.gif" alt="" />Nullamlacus dui ipsum conseque loborttis</h2>
+          <p>Nullamlacus dui ipsum conseque loborttis non euisque morbi penas dapibulum orna. Urnaultrices quis curabitur phasellentesque congue magnis vestibulum quismodo nulla et feugiat. Adipisciniapellentum leo ut consequam ris felit elit id nibh sociis malesuada.</p>
+          <p class="readmore"><a href="#">Selengkapnya &raquo;</a></p>
+        </li>
+        <li class="last">
+          <h2><img src="images/demo/60x60.gif" alt="" />Nullamlacus dui ipsum conseque loborttis</h2>
+          <p>Nullamlacus dui ipsum conseque loborttis non euisque morbi penas dapibulum orna. Urnaultrices quis curabitur phasellentesque congue magnis vestibulum quismodo nulla et feugiat. Adipisciniapellentum leo ut consequam ris felit elit id nibh sociis malesuada.</p>
+          <p class="readmore"><a href="#">Selengkapnya &raquo;</a></p>
+        </li>
+      </ul>
+      <br class="clear" />
+    </div>
+  </div>
+</div>
+<!-- ####################################################################################################### -->
+<div class="wrapper col4">
+  <div id="footer">
+    <div class="box1">
+      <h2>A Little Company Information !</h2>
+      <img class="imgl" src="images/demo/imgl.gif" alt="" />
+      <p>Morbitincidunt maurisque eros molest nunc anteget sed vel lacus mus semper. Anterdumnullam interdum eros dui urna consequam ac nisl nullam ligula vestassa. Condimentumfelis et amet tellent quisquet a leo lacus nec augue</p>
+      <p>Portortornec condimenterdum eget consectetuer condis.</p>
+    </div>
+    <div class="box contactdetails">
+      <h2>Our Contact Details !</h2>
+      <ul>
+        <li>Company Name</li>
+        <li>Street Name &amp; Number</li>
+        <li>Town</li>
+        <li>Postcode/Zip</li>
+        <li>Tel: xxxxx xxxxxxxxxx</li>
+        <li>Fax: xxxxx xxxxxxxxxx</li>
+        <li>Email: info@domain.com</li>
+        <li class="last">LinkedIn: <a href="#">Company Profile</a></li>
+      </ul>
+    </div>
+    <div class="box flickrbox">
+      <h2>Latest Flickr Images !</h2>
+      <div class="wrap">
+        <div class="fix"></div>
+        <div class="flickr_badge_image" id="flickr_badge_image1"><a href="#"><img src="images/demo/80x80.gif" alt="" /></a></div>
+        <div class="flickr_badge_image" id="flickr_badge_image2"><a href="#"><img src="images/demo/80x80.gif" alt="" /></a></div>
+        <div class="flickr_badge_image" id="flickr_badge_image3"><a href="#"><img src="images/demo/80x80.gif" alt="" /></a></div>
+        <div class="flickr_badge_image" id="flickr_badge_image4"><a href="#"><img src="images/demo/80x80.gif" alt="" /></a></div>
+        <div class="flickr_badge_image" id="flickr_badge_image5"><a href="#"><img src="images/demo/80x80.gif" alt="" /></a></div>
+        <div class="flickr_badge_image" id="flickr_badge_image6"><a href="#"><img src="images/demo/80x80.gif" alt="" /></a></div>
+        <div class="fix"></div>
+      </div>
+    </div>
+    <br class="clear" />
+  </div>
+</div>
+<!-- ####################################################################################################### -->
+<div class="wrapper col5">
+  <div id="copyright">
+    <p class="fl_left">Copyright &copy; 2014 - All Rights Reserved - <a href="#">Domain Name</a></p>
+    <p class="fl_right">Template by <a target="_blank" href="http://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p>
+    <br class="clear" />
+  </div>
+</div>
+</body>
+</html>
