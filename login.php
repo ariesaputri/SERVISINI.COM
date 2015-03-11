1065461c218c8603ccf662d9f7d155f53192913a
ariesaputri <ariesaputri87@gmail.com>
2015-03-11 11:58:01 +0700

    Create login.php
    form login admin

---
+++ login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+  <meta charset="UTF-8">
+
+  <title>Admin Login</title>
+
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+  <div class="login-wrap">
+  <h2>Halaman Login Admin</h2>
+
+  <div class="form">
+  	<form action="proseslogin.php" method="POST">
+    	<input type="text" placeholder="Username" name="Username" />
+    	<input type="password" placeholder="Password" name="Password" />
+    	<button input type="Submit"]/>Login</button>
+    </form>
+  </div>
+</div>
+
+  <script src='https://code.jquery.com/jquery-1.10.0.min.js'></script>
+
+  <script src="js/index.js"></script>
+
+</body>
+
+</html>
